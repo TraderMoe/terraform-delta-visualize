e7c1bd026b4207c9e4be6a71acbd61ca557751b9
Maurice Ollenburg <ollenbum@apetito.com>
2023-12-07 15:59:04 +0100

    bugfix github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -41,5 +44,24 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(fileName) {
+          	const match = DRIVE_LETTER_REGEX.exec(fileName);
+            const driveLetter = match ? match[0] : "";
+
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return (
+              driveLetter +
+              fileName
+                .substring(driveLetter.length)
+                .replace(INVALID_CHAR_REGEX, "")
+            );
+        },
+      },
+    },
   },
 });
